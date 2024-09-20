0a777e26d23706d7eb68548ce98f2e60729d0584
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-09-21 01:09:55 +0600

    main.go created and application and server struct created

---
+++ cmd/web/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/CloudyKit/jet/v6"
+	"log"
+)
+
+type server struct {
+	host string
+	port string
+	url  string
+}
+type application struct {
+	appName string
+	server  server
+	debug   bool
+	errLog  *log.Logger
+	infoLog *log.Logger
+	view    *jet.Set
+}
+
+func main() {
+
+}
